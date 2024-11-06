de53d6955ef52d78e80b26e1927ef177e77bffe1
Lotanna <lobiekwe@students.kennesaw.edu>
2024-11-05 22:51:34 -0500

    milestone 2 added

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-// This alert will welcome the user to the website
-alert("Welcome to my Website!")
+
+const express = require('express');
+const path = require('path')
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
+// Define a route for the home page
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'));
+});
+// Define a route for the about page
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, '/about.html'));
+});
+// Define a route for the contact page
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, '/contact.html'));
+  });
+// Start the server
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
