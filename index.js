cc6ecc46871b7cc2dcfa5327787200ec4b61f9ef
Dorian Taponzing <thebreezyguy@Dorians-MacBook-Pro.local>
2024-11-12 23:42:53 -0500

    Changes in CSS and contact form

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
-
-const express = require('express');
-const path = require('path')
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
 const app = express();
 
-app.use(express.static(__dirname + '/public'));
+// Middleware to parse form data
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(__dirname + "/public"));
+app.use("/images", express.static(path.join(__dirname, "images")));
+
 // Define a route for the home page
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/index.html"));
 });
+
+app.post("/contact", (req, res) => {
+  const { name, email, message } = req.body;
+  console.log(`Name: ${name}, Email: ${email}, Message: ${message}`);
+  res.send("Form submitted successfully!");
+});
+
 // Define a route for the about page
-app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, '/about.html'));
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "/about.html"));
 });
 // Define a route for the contact page
-app.get('/contact', (req, res) => {
-    res.sendFile(path.join(__dirname, '/contact.html'));
-  });
+app.get("/contact", (req, res) => {
+  res.sendFile(path.join(__dirname, "/contact.html"));
+});
 // Start the server
 app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+  console.log("Server listening on port 3000");
 });
